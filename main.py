3dee5111e8df3ef6cc69652d8dbdc3d67e9bc1e5
Jakub Brambor <kuba.brambor@gmail.com>
2020-06-20 10:47:11 +0200

    adding flask-bootstrap to templates

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from index.second import second
 from contact.contact import contact
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 app.register_blueprint(second, url_prefix="")
 app.register_blueprint(contact, url_prefix="/contact")
 
