e96ac3aca86825f7a0ce432c60770f0356895f05
Jakub Brambor <kuba.brambor@gmail.com>
2020-06-20 14:46:44 +0200

    linked stylesheets to modules and fixed path problems

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from index.second import second
+from home.home import home
 from contact.contact import contact
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
-app.register_blueprint(second, url_prefix="")
+app.register_blueprint(home, url_prefix="/home")
 app.register_blueprint(contact, url_prefix="/contact")
 
 
