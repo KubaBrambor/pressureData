fa445ca536ce2545ec3b28ef502246cbbfb33d31
Jakub Brambor <kuba.brambor@gmail.com>
2020-07-15 21:21:10 +0200

    deleting all

--- contact/contact.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-contact = Blueprint("contact", __name__, static_folder="static", template_folder="templates")
-
-@contact.route('/')
-def main():
-    return render_template('contact.html')
--- data/data.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-data = Blueprint("data", __name__, static_folder="static", template_folder="templates")
-
-@data.route("/")
-def index():
-    return render_template("data.html")
-
